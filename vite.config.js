e20bb445efaa722a7ee75cab1437b99d8e714e54
Altamash <altamashtauheed501@gmail.com>
2024-08-25 14:03:02 +0530

    added proxy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/post':'https://full-stack-blog-lyart.vercel.app'
+    },
+  },
   plugins: [react()],
 })
