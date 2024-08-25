6c53eabba6dac53876ffb01afe6797ab36aab6df
Altamash <altamashtauheed501@gmail.com>
2024-08-25 14:11:53 +0530

    added vercel.json

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/post':'https://full-stack-blog-lyart.vercel.app'
-    },
-  },
   plugins: [react()],
 })
